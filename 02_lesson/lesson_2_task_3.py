586307b224d9b79c2eca885ad97b304dfd8211b6
Ekaterina1878 <lapochka1878@gmail.com>
2025-07-07 16:04:51 +0300

    create branch

--- 02_lesson/lesson_2_task_3.py
+++ 02_lesson/lesson_2_task_3.py
@@ -1,5 +1,9 @@
 import math
+
+
 def square(x):
     return math.ceil(x*x)
+
+
 num_x = float(input("Введите длину стороны квадрата: "))
 print(f"Площадь квадрата: {square(num_x)}")
--- 02_lesson/lesson_2_task_4.py
+++ 02_lesson/lesson_2_task_4.py
@@ -1,5 +1,5 @@
 def fizz_buzz(n):
-    for x in range(1, n+1):
+    for x in range(1, n + 1):
         if x % 3 == 0 and x % 5 == 0:
             print("FizzBuzz")
         elif x % 3 == 0:
@@ -8,5 +8,7 @@ def fizz_buzz(n):
             print("Buzz")
         else:
             print(x)
+
+            
 n = int(input("Введите чило: "))
 fizz_buzz(n)
