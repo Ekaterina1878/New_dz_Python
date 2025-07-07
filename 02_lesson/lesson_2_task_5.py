716368d0e3b3724a3ec6902be22fe74592f02075
Ekaterina1878 <lapochka1878@gmail.com>
2025-07-07 11:41:20 +0300

    create branch

---
+++ 02_lesson/lesson_2_task_5.py
@@ -0,0 +1,13 @@
+num = int(input("Введите номер месяца от 1 до 12: "))
+def month_to_season(num):
+    if 1 <= num <=2 or num ==12:
+        return "Зима"
+    elif 3 <= num <=5:
+        return "Весна"
+    elif 6 <= num <=8:
+        return "Лето"
+    elif 9 <= num <=11:
+        return "Осень"
+    else:
+        return "Неверный номер месяца"
+print(month_to_season(num))
