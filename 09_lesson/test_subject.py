e8f3eb7aca91255871645ae51f53d32a9d758d22
Ekaterina1878 <lapochka1878@gmail.com>
2025-09-23 19:52:18 +0300

    Изменения в папке 09_lesson, gitignore

--- 09_lesson/test_subject.py
+++ 09_lesson/test_subject.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from SubjectPage import SubjectPage
+from subject_page import SubjectPage
 
 db_connection_string = "postgresql://postgres:здесь должен быть пароль@localhost:5432/QA"
 db = create_engine(db_connection_string)
